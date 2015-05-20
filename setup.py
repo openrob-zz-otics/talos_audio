21d2e5a60ad4e0dfb4c4f4b6e9526f1bfa3b8d41
dash <dash@dash-ThinkPad-T530.(none)>
2015-05-20 08:37:13 -0400

    Now installs to talos_audio for python

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['talos_speech'],
+    packages=['talos_audio'],
     package_dir={'': 'src'},
     requires=['std_msgs', 'rospy']
 )
